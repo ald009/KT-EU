9ee08feddc0f196c3f2c063ab601519c58e696c3
Kovács Tibor <kovacs.tibor@diak.szbi-pg.hu>
2026-09-15 14:59:56 +0200

    elso

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KT_EU
 {
+
+    class Eu
+    {
+        public Eu() {
+
+
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
+
+
         }
     }
 }
